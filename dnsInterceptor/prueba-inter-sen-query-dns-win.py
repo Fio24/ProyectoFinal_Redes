26f89f73b346f3a3bddc7111c2e573572439c803
Leonardo David Fariña Ozamis <65524519+LeonardoFarina@users.noreply.github.com>
2023-06-13 14:29:02 -0600

    Cambio puertos

--- dnsInterceptor/prueba-inter-sen-query-dns-win.py
+++ dnsInterceptor/prueba-inter-sen-query-dns-win.py
@@ -5,7 +5,7 @@ resolver = dns.resolver.Resolver()
 
 # Establece el servidor DNS al que enviar la consulta (opcional, usa el predeterminado si no se establece)
 resolver.nameservers = ['127.0.0.1']
-resolver.port = 8080  # Establece el puerto 8080
+resolver.port = 53  # Establece el puerto 53
 
 # Realiza la consulta DNS
 respuesta = resolver.resolve('example.com', 'A')
