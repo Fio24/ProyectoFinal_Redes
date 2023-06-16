fb731593cf922c524e2feb0b1d8af1e6ce52e3eb
Esteban Durán <eiduran@estudiantec.cr>
2023-06-15 18:59:21 -0600

    To DNS server changed with DNS.resolver library

--- dnsapi/main.py
+++ dnsapi/main.py
@@ -1,8 +1,9 @@
 import base64
-import dns.query
+import dns.resolver
 import csv
 import ipaddress
 import time
+
 
 from flask import Flask, jsonify, request
 import json
@@ -96,6 +97,7 @@ def dns_resolver():
 
 @app.route('/dns_request', methods=['POST'])
 def dns_request():
+    """
     query_data_base64 = request.get_data()
 
     # Decodificar datos base64
@@ -112,8 +114,30 @@ def dns_request():
 
     #return response_data_base64, 200, {'Content-Type': 'application/dns-message'}
     return response_data_base64
+    """
+    # Crea un objeto de resolución de DNS
+    resolver = dns.resolver.Resolver()
 
+    # Establece el servidor DNS al que enviar la consulta (opcional, usa el predeterminado si no se establece)
+    resolver.nameservers = [DNS_EXTERNAL_SERVER]
+    resolver.port = 53  # Establece el puerto 53
+    try:
+        # Realiza la consulta DNS
+        respuesta = resolver.resolve('esteban.com', 'A')
+    except dns.exception.DNSException as e:
+        import traceback
+        traceback.print_exc()
+        print("Error performing DNS query:", e)
 
+    # Verifica si la respuesta se obtuvo correctamente
+    if respuesta:
+        # Accede a los bytes de la respuesta DNS
+        wire_bytes = respuesta.response.to_wire()
+        # Imprime los bytes de la respuesta
+        print("Bytes de respuesta:", wire_bytes)
+        return base64.b64encode(wire_bytes).decode('utf-8')
+    else:
+        print("Error")
 
 # Endpoint to retrieve all data
 @app.route('/api/write_json', methods=['POST'])
